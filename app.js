0b07c8f49cf84b2814384d010a779c089825d6b8
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2017-12-15 22:47:45 +0200

    first commit with module patterns

---
+++ app.js
@@ -0,0 +1,5 @@
+let stuff = require('./stuff');
+
+let names = ['Shoaib', 'John', 'Sarah', 'Kate'];
+console.log(stuff.counter(names));
+console.log(stuff.sum(5, 5));
---
+++ stuff.js
@@ -0,0 +1,5 @@
+module.exports.counter = (arr) => {
+  return 'There are ' + arr.length + ' elments in this array.'
+}
+
+module.exports.sum = (a, b) => `Sum of ${a} and ${b} is = ${a + b}`;

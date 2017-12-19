96ca60b58cbe41903db5ae089b6630427524fe1f
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2017-12-19 21:21:36 +0200

    introducing pipe

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
+const http = require('http');
 const fs = require('fs');
 
-let myReadStream = fs.createReadStream(__dirname + '/chunk.txt', 'utf8');
-let myWriteStream = fs.createWriteStream(__dirname + '/WrittenChunk.txt');
-myReadStream.on('data', (pieceOfData) => {
-  console.log('Buffer recieved: ');
-  myWriteStream.write(pieceOfData);
-});
-
-/*
-const http = require('http');
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
   console.log("This request is made: " + req.url);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('This is a response body.');
+  let myReadStream = fs.createReadStream(__dirname + '/chunk.txt', 'utf8');
+  myReadStream.pipe(res);
+
 });
 
-server.listen(3009,'127.0.0.1');
+server.listen(3009, '127.0.0.1');
 console.log('Listening to port 3009');
-*/

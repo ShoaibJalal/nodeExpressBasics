e62f6eb3c9cda5c251a36e93e7bbc56b85128943
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2017-12-19 22:12:04 +0200

    Serving HTML pages

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log("This request is made: " + req.url);
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  let myReadStream = fs.createReadStream(__dirname + '/chunk.txt', 'utf8');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  let myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
   myReadStream.pipe(res);
 
 });

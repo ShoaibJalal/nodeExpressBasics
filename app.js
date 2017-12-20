b014c3f3f387674360075d4ebb240d39c9517e28
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2017-12-20 10:31:45 +0200

    Intro to Basic Routing

--- app.js
+++ app.js
@@ -3,13 +3,31 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   console.log("This request is made: " + req.url);
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  let infoObject = {
-    name: 'shoaib',
-    role: 'developer',
-    age: 21
+
+  if (req.url === '/home' || req.url === '/') {
+    res.writeHead(200, {'content-type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/about') {
+    res.writeHead(200, {'content-type': 'text/html'});
+    fs.createReadStream(__dirname + '/about.html').pipe(res);
+
+  } else if (req.url === '/api/developers') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    let infoObject = [{
+        name: 'shoaib',
+        role: 'developer',
+        age: 21
+      }, {
+        name: 'John',
+        role: 'designer',
+        age: 27
+      }]
+    res.end(JSON.stringify(infoObject));
+
+  } else {
+    res.writeHead(404, {'content-type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
   }
-  res.end(JSON.stringify(infoObject));
 
 });
 

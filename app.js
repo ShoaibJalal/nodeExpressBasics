35ce9048e09843aa4fe8a1472b1e3d31f40b5476
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2017-12-21 10:12:49 +0200

    Intro to Express Route Params

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
   res.send('U r at Home Page.');
 });
 
+app.get('/student/:id', (req, res) => {
+  res.send('You are seeing a student with ID of : ' + req.params.id);
+});
+
 app.listen(3001);

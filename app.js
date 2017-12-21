b36fabeff12be368dfc9608bd99320ba415700d0
Shoaib Jalal <shoaib.jalal@myy.haaga-helia.fi>
2017-12-21 10:02:37 +0200

    Intro to Express module

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-  console.log("This request is made: " + req.url);
+let app = express();
 
-  if (req.url === '/home' || req.url === '/') {
-    res.writeHead(200, {'content-type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if (req.url === '/about') {
-    res.writeHead(200, {'content-type': 'text/html'});
-    fs.createReadStream(__dirname + '/about.html').pipe(res);
-
-  } else if (req.url === '/api/developers') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    let infoObject = [{
-        name: 'shoaib',
-        role: 'developer',
-        age: 21
-      }, {
-        name: 'John',
-        role: 'designer',
-        age: 27
-      }]
-    res.end(JSON.stringify(infoObject));
-
-  } else {
-    res.writeHead(404, {'content-type': 'text/html'});
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
-
+app.get('/', (req, res) => {
+  res.send('U r at Home Page.');
 });
 
-server.listen(3009, '127.0.0.1');
-console.log('Listening to port 3009');
+app.listen(3001);
